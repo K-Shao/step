e983c8b3e55c03392e9e52050fdc614ef5d6888a
Kevin Shao <knshao@gmail.com>
2020-06-25 22:20:58 +0000

    Add navigation bar to all HTML files

---
+++ portfolio/src/main/webapp/nav.js
@@ -0,0 +1,9 @@
+
+document.write('\
+  <div class="topnav">\
+    <a class="active" href="index.html">Home</a>\
+    <a href="hobbies.html">Hobbies</a>\
+    <a href="programming.html">Programming</a>\
+  </div> \
+  '
+);
