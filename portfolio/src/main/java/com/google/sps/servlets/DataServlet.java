106db60abad0a77fc5d0585a244c60b1124436c0
Kevin Shao <knshao@gmail.com>
2020-07-06 20:13:03 +0000

    Add limiting comments to servlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -53,11 +53,13 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    int limit = Integer.parseInt(request.getParameter("limit"));
     Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
     ImmutableList<String> comments = 
                             Streams.stream(results.asIterable())
+                            .limit(limit)
                             .map(entity -> entity.getProperty("content").toString())
                             .collect(toImmutableList());
 
